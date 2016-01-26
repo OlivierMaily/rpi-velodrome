93691fabe8421365c7c864e35e7a4598c7188d03
Regulvar-OM <omaily@regulvar.com>
2016-01-26 12:12:57 -0500

    Update modification.sh

--- modification.sh
+++ modification.sh
@@ -1,6 +1,6 @@
 #!/bin∕bash
 cd '/media/f24a4949-f4b2-4cad-a780-a138695079ec/home/pi' 
 sudo rm cpia_velodrome.py
-sudo wget https://raw.githubusercontent.com/Regulvar-OM/rpi-velodrome/master/cpia_velodrome.py
+sudo wget https://raw.githubusercontent.com/Regulvar-OM/rpi-velodrome/775cee379eaaaea8119ea7d65801b855cb7607cd/cpia_velodrome.py
 sudo umount /dev/mmcblk0p1
 sudo umount /dev/mmcblk0p2
